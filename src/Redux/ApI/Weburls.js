a8126be8d7cd033ed10863da64178a2a03423adc
Vikaskumar8085 <vikaskumar7049002404@gmail.com>
2023-03-09 14:46:36 +0530

    frontend

---
+++ src/Redux/ApI/Weburls.js
@@ -0,0 +1,30 @@
+const Weburls = {
+  galleryapi: "http://192.168.1.34:3000/api/v1/Galleryget",
+  Bloggetapi: "http://192.168.1.34:3000/api/v1/getapi",
+  Careergetapi: "http://localhost:3000/api/v1/Careerget",
+  CareerPostapi: "http://localhost:3000/api/v1/CareerPost",
+  Newsgetapi: "http://192.168.1.34:3000/api/v1/Newsget",
+  Productgetapi: "http://192.168.1.34:3000/api/v1/Productget",
+  Projectgetapi: "http://192.168.1.34:3000/api/v1/Projectget",
+  ContactPostapi: "http://192.168.1.34:3000/api/v1/ContactPost",
+  SliderGetapi: "http://192.168.1.34:3000/api/v1/SliderGet",
+  SubPostapi: "http://192.168.1.34:3000/api/v1/SubPost",
+
+  Videoapi: "http://192.168.1.34:3000/api/v1/VideoGet",
+
+  // Product api
+
+  ProductWooden: "http://192.168.1.34:3000/api/v1/ProductWoodenget",
+  ProductGear: "http://192.168.1.34:3000/api/v1/Productgearbox",
+  ProductFRP: "http://192.168.1.34:3000/api/v1/ProductFRPget",
+  ProductFRPFan: "http://192.168.1.34:3000/api/v1/ProductFRPFan",
+  ProductRcc: "http://192.168.1.34:3000/api/v1/ProductRccTower",
+
+  //gallery api
+  gallerygetapi3: "http://192.168.1:34:3000/api/v1/Galleryget3",
+  gallerygetapi2: "http://192.168.1.34:3000/api/v1/Galleryget2",
+  gallerygetapi1: "http://192.168.1.34:3000/api/v1/Galleryget1",
+  gallerygetapi: "http://192.168.1.34:3000/api/v1/Galleryget",
+};
+
+export default Weburls;
